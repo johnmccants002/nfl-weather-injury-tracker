d03ba703bd0dc419ab8bad7044cc3bd4f20cdfd9
John McCants <47401369+johnmccants002@users.noreply.github.com>
2024-11-06 07:36:57 -0800

    update

--- jobs/injuryUpdateJob.js
+++ jobs/injuryUpdateJob.js
@@ -1,6 +1,6 @@
 const cron = require("node-cron");
 const axios = require("axios");
-const CurrentWeek = require("../models/CurrentWeek"); // Import the model for the current week
+const connectToDatabase = require("../db/conn"); // Import the database connection function
 
 cron.schedule(
   "0 9 * * *",
@@ -8,20 +8,22 @@ cron.schedule(
     try {
       console.log("Running daily injury update");
 
-      // Fetch the current week from MongoDB
-      const currentWeek = await CurrentWeek.findOne({});
+      // Connect to the database and fetch the current week
+      const db = await connectToDatabase();
+      const currentWeek = await db.collection("currentWeek").findOne({});
+
       if (!currentWeek) {
         throw new Error("Current week not found in the database");
       }
 
       // Define the API parameters
       const locale = "en";
-      const year = 2024; // You could dynamically set this to the current year if needed
+      const year = 2024; // Optionally, dynamically set this to the current year if needed
       const seasonType = "REG";
       const weekNumber = currentWeek.sequence; // Use the current week number from MongoDB
 
       // Construct the URL with query parameters
-      const url = `http://localhost:3000/api/injuries?locale=${locale}&year=${year}&season_type=${seasonType}&week_number=${weekNumber}`;
+      const url = `https://nfl-weather-injury-tracker-48b3c17d4980.herokuapp.com/injuries?locale=${locale}&year=${year}&season_type=${seasonType}&week_number=${weekNumber}`;
 
       // Make the request to update injury data
       await axios.get(url);
